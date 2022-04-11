85f5498442d61cb694c4910d9a0e51f178d4b0ff
Koen Serneels <ks@error.be>
2022-04-11 13:29:14 +0200

    Obsolete, release via pipeline

--- create_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-tag=$(date +'%Y%m%dH%H%M')
-
-
-
-cp SmartEVSE-3/.pio/build/release/firmware.bin releases/firmware_V3a_${tag}.bin
-cp SmartEVSE-3/.pio/build/release/spiffs.bin releases/spiffs.bin
