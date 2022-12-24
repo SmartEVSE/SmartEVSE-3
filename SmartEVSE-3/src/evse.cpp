ad1f1f7cf77521f78b760cca3db67655b65597e0
dingo35 <spaarder@hotmail.nl>
2022-12-24 13:33:41 +0100

    evse.cpp: start debug server with debuglevel INFO

--- SmartEVSE-3/src/evse.cpp
+++ SmartEVSE-3/src/evse.cpp
@@ -3310,7 +3310,7 @@ void WiFiSetup(void) {
 
 #if DBG != 0
     // Initialize the server (telnet or web socket) of RemoteDebug
-    Debug.begin(APhostname);
+    Debug.begin(APhostname, 23, 3);
     Debug.showColors(true); // Colors
 #if DBG == 2
     Debug.setSerialEnabled(true); // if you wants serial echo - only recommended if ESP is plugged in USB
