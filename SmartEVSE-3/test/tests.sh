0bfc4e1fa9095e03376c31f2d81634c4e2967868
dingo35 <spaarder@hotmail.nl>
2024-01-21 09:56:29 -0500

    tests.sh: add socket hardwiring test

--- SmartEVSE-3/test/tests.sh
+++ SmartEVSE-3/test/tests.sh
@@ -10,6 +10,11 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+# please give values in deci-Ampère:
+MASTER_SOCKET_HARDWIRED=320
+SLAVE_SOCKET_HARDWIRED=130
+
+
 MASTER="smartevse-"$1".local"
 SLAVE="smartevse-"$2".local"
 
@@ -31,11 +36,6 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
 ABORT=0
-#read maxcurrent out of settings:
-#curl -s -X GET http://smartevse-$MASTER.local/settings | jq ".settings.current_max"
-
-#set maxcurrent:
-#curl -X POST http://192.168.2.181/automated_testing?current_max=10
 
 #MODESWITCH TEST: test if mode changes on master reflects on slave and vice versa
 $CURLPOST $MASTER/automated_testing?loadbl=1
@@ -73,17 +73,68 @@ if [ $ABORT -ne 0 ]; then
     exit $ABORT
 fi
 
-exit 0
+
+#SOCKET HARDWIRING TEST: test if Socket resistors in test bench limit our current // Configuration (0:Socket / 1:Fixed Cable)
+#                        needs setting [MASTER|SLAVE]_SOCKET_HARDWIRED to the correct values of your test bench
+
+#first load all settings before the test
+for device in $MASTER $SLAVE; do
+    $CURLPOST $device/automated_testing?config=0
+    $CURLPOST $device/automated_testing?config=0
+    #save MaxCurrent setting and set it very high
+    MAXCUR=$(curl -s -X GET $device/settings | jq ".settings.current_max")
+    $CURLPOST $device/automated_testing?current_max=60
+    #save MaxCircuit setting and set it very high
+    MAXCIRCUIT=$(curl -s -X GET $device/settings | jq ".settings.current_max_circuit")
+    $CURLPOST $device/automated_testing?current_max_circuit=70
+    #save MaxMains setting and set it very high
+    MAXMAINS=$(curl -s -X GET $device/settings | jq ".settings.current_main")
+    $CURLPOST $device/automated_testing?current_main=80
+done
+
+read -p "Make sure all EVSE's are set to NOT charging, then CHARGING, then press <ENTER>" dummy
 
 for loadbl_master in 0 1; do
-    curl -X POST $MASTER/automated_testing?loadbl=$loadbl_master
+    $CURLPOST $MASTER/automated_testing?loadbl=$loadbl_master
+    if [ $loadbl_master -eq 1 ]; then
+        loadbl_slave=2
+    else
+        loadbl_slave=0
+    fi
+    $CURLPOST $SLAVE/automated_testing?loadbl=$loadbl_slave
+    #if we are in loadbl 0 we test the slave device in loadbl 0 also
     for mode_master in 1 2 3; do
-        curl -X POST $MASTER/settings?mode=$mode_master
-        sleep 3
-        LBL=$(curl -s -X GET $MASTER/settings | jq ".evse.loadbl")
+        $CURLPOST $MASTER/settings?mode=$mode_master
+        if [ $loadbl_slave -eq 0 ]; then
+            $CURLPOST $SLAVE/settings?mode=$mode_master
+        fi
+        #settle switching modes AND stabilizing charging speeds
+        #LBL=$(curl -s -X GET $MASTER/settings | jq ".evse.loadbl")
         MODE=$(curl -s -X GET $MASTER/settings | jq ".mode")
-        echo LOADBL=$LBL, MODE=$MODE
+        #echo LOADBL=$LBL, MODE=$MODE
+        printf "Testing with LBL=$loadbl_master, mode=$MODE.\r"
+        sleep 10
+        CHARGECUR_M=$(curl -s -X GET $MASTER/settings | jq ".settings.charge_current")
+        if [ $CHARGECUR_M -eq $MASTER_SOCKET_HARDWIRED ]; then
+            printf "$Green Passed $NC LBL=$loadbl_master, Mode=$MODE: Master chargecurrent is limited to socket hardwiring.\n"
+        else
+            printf "$Red Failed $NC LBL=$loadbl_master, Mode=$MODE: Master chargecurrent is $CHARGECUR_M dA and should be limited to $MASTER_SOCKET_HARDWIRED dA.\n"
+        fi
+        CHARGECUR_S=$(curl -s -X GET $SLAVE/settings | jq ".settings.charge_current")
+        if [ $CHARGECUR_S -eq $SLAVE_SOCKET_HARDWIRED ]; then
+            printf "$Green Passed $NC LBL=$loadbl_slave, Mode=$MODE: Slave chargecurrent is limited to socket hardwiring.\n"
+        else
+            printf "$Red Failed $NC LBL=$loadbl_slave, Mode=$MODE: Slave chargecurrent is $CHARGECUR_S dA and should be limited to $SLAVE_SOCKET_HARDWIRED dA.\n"
+        fi
     done
 done
 
+#for all other tests we don't want socket resistors to limit our currents, so switch to Fixed Cable
+for device in $MASTER $SLAVE; do
+    $CURLPOST $device/automated_testing?config=1
+    $CURLPOST $device/automated_testing?config=1
+    #TODO do we want to restore all MaxCurrent/MaxCircuit/MaxMains values?
+done
+
+exit 0
 
