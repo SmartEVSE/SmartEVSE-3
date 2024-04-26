de0ce75068260220a2888e23530b820c61bf1106
dingo35 <spaarder@hotmail.nl>
2024-04-26 08:55:58 +0200

    packfs.py: make build process independent of os gzip

--- SmartEVSE-3/packfs.py
+++ SmartEVSE-3/packfs.py
@@ -1,5 +1,5 @@
 #this script will be run by platformio.ini from its native directory
-import os, sys
+import os, sys, gzip
 
 #check for the two files we need to be able to keep updating the firmware by the /update endpoint:
 if not os.path.isfile("data/update2.html"):
@@ -11,10 +11,18 @@ os.system('mkdir pack.tmp')
 #    sys.exit(3)
 if os.system('cp -a data pack.tmp'):
     sys.exit(5)
-if os.system('gzip pack.tmp/data/*'):
-    sys.exit(6)
-if os.system('gunzip pack.tmp/data/zones.csv'):
-    sys.exit(7)
+
+# now gzip the stuff except zones.csv since this file is not served by mongoose but directly accessed:
+for file in os.listdir("pack.tmp/data"):
+    filename = os.fsdecode(file)
+    if not filename == "zones.csv":
+        with open('pack.tmp/data/' + filename, 'rb') as f_in, gzip.open('pack.tmp/data/' + filename + '.gz', 'wb') as f_out:
+            f_out.writelines(f_in)
+            os.remove('pack.tmp/data/' + filename)
+        continue
+    else:
+        continue
+
 if os.system('cd pack.tmp; python ../pack.py data/* >../src/packed_fs.c'):
     sys.exit(8)
 if os.system('rm -rf pack.tmp'):
