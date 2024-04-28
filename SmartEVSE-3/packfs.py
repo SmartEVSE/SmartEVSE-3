342467eb35c5610b7571fd0aa4c238071eae4da7
dingo35 <spaarder@hotmail.nl>
2024-04-28 09:58:36 +0200

    packfs.py: avoid unnecessary file copying

--- SmartEVSE-3/packfs.py
+++ SmartEVSE-3/packfs.py
@@ -12,23 +12,23 @@ if os.path.isdir("pack.tmp"):
     shutil.rmtree('pack.tmp')
 try:
     filelist = []
-    shutil.copytree('data', 'pack.tmp/data')
+    os.makedirs('pack.tmp/data')
     # now gzip the stuff except zones.csv since this file is not served by mongoose but directly accessed:
-    for file in os.listdir("pack.tmp/data"):
+    for file in os.listdir("data"):
         filename = os.fsdecode(file)
         if not filename == "zones.csv":
-            with open('pack.tmp/data/' + filename, 'rb') as f_in, gzip.open('pack.tmp/data/' + filename + '.gz', 'wb') as f_out:
+            with open('data/' + filename, 'rb') as f_in, gzip.open('pack.tmp/data/' + filename + '.gz', 'wb') as f_out:
                 f_out.writelines(f_in)
             filelist.append('data/' + filename + '.gz')
-            os.remove('pack.tmp/data/' + filename)
             continue
         else:
+            shutil.copy('data/' + filename, 'pack.tmp/data/' + filename)
             filelist.append('data/' + filename)
             continue
     os.chdir('pack.tmp')
-    cmdstring = ' '.join(filelist)
-    cmdstring = 'python ../pack.py ' + cmdstring
+    cmdstring = 'python ../pack.py ' + ' '.join(filelist)
     os.system(cmdstring + '>../src/packed_fs.c')
+    os.chdir('..')
 except Exception as e:
     print(f"An error occurred: {str(e)}")
     sys.exit(100)
