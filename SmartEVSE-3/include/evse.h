bdf1db2de27b55504cc9fc6a4a4645ce2abaf683
Michael Stegen <michael@stegen.com>
2024-06-21 19:48:09 +0200

    Remove extra newline on serial debug output

--- SmartEVSE-3/include/evse.h
+++ SmartEVSE-3/include/evse.h
@@ -111,38 +111,40 @@
 extern RemoteDebug Debug;
 #endif
 
+#define EVSE_LOG_FORMAT(letter, format) "[%6u][" #letter "][%s:%u] %s(): " format , (uint32_t) (esp_timer_get_time() / 1000ULL), pathToFileName(__FILE__), __LINE__, __FUNCTION__
+
 #if DBG == 2
 #define DEBUG_DISABLED 1
 #if LOG_LEVEL >= 1  // Errors
-#define _LOG_A( ... ) log_n ( __VA_ARGS__ )
+#define _LOG_A(fmt, ... ) Serial.printf(EVSE_LOG_FORMAT(E, fmt), ##__VA_ARGS__)
 #define _LOG_A_NO_FUNC( ... ) Serial.printf ( __VA_ARGS__ )
 #else
 #define _LOG_A( ... )
 #define _LOG_A_NO_FUNC( ... )
 #endif
 #if LOG_LEVEL >= 2  // Warnings
-#define _LOG_W( ... ) log_w ( __VA_ARGS__ )
+#define _LOG_W(fmt, ... ) Serial.printf(EVSE_LOG_FORMAT(W, fmt), ##__VA_ARGS__)
 #define _LOG_W_NO_FUNC( ... ) Serial.printf ( __VA_ARGS__ )
 #else
 #define _LOG_W( ... ) 
 #define _LOG_W_NO_FUNC( ... )
 #endif
 #if LOG_LEVEL >= 3  // Info
-#define _LOG_I( ... ) log_i ( __VA_ARGS__ )
+#define _LOG_I(fmt, ... ) Serial.printf(EVSE_LOG_FORMAT(I, fmt), ##__VA_ARGS__)
 #define _LOG_I_NO_FUNC( ... ) Serial.printf ( __VA_ARGS__ )
 #else
 #define _LOG_I( ... )
 #define _LOG_I_NO_FUNC( ... )
 #endif
 #if LOG_LEVEL >= 4  // Debug
-#define _LOG_D( ... ) log_d ( __VA_ARGS__ )
+#define _LOG_D(fmt, ... ) Serial.printf(EVSE_LOG_FORMAT(D, fmt), ##__VA_ARGS__)
 #define _LOG_D_NO_FUNC( ... ) Serial.printf ( __VA_ARGS__ )
 #else
 #define _LOG_D( ... ) 
 #define _LOG_D_NO_FUNC( ... )
 #endif
 #if LOG_LEVEL >= 5  // Verbose
-#define _LOG_V( ... ) log_v ( __VA_ARGS__ )
+#define _LOG_V(fmt, ... ) Serial.printf(EVSE_LOG_FORMAT(V, fmt), ##__VA_ARGS__)
 #define _LOG_V_NO_FUNC( ... ) Serial.printf ( __VA_ARGS__ )
 #else
 #define _LOG_V( ... ) 
