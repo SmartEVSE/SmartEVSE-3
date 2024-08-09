a3ab570f18e9906fe5c2247ea56818b47a30d996
rob040 <rob040@hotmail.nl>
2024-08-09 10:29:57 +0200

    RemoteDebug.cpp: do not emit any (COLOR_RESET) escape codes when color mode is disabled

--- SmartEVSE-3/lib/RemoteDebug/src/RemoteDebug.cpp
+++ SmartEVSE-3/lib/RemoteDebug/src/RemoteDebug.cpp
@@ -1194,6 +1194,7 @@ size_t RemoteDebug::write(uint8_t character) {
 		if (noPrint == false) {
 
 #ifdef COLOR_NEW_SYSTEM
+			if (_showColors)
 			_bufferPrint.concat(COLOR_RESET);
 #endif
 			// Send to telnet or websocket (buffered)
