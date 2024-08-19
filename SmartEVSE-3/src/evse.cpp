1409bb67338b445b7585bfed54c4c162d833c139
rob040 <rob040@hotmail.nl>
2024-08-19 17:22:26 +0200

    evse.cpp: improve readability MaxSumMains constraint

--- SmartEVSE-3/src/evse.cpp
+++ SmartEVSE-3/src/evse.cpp
@@ -1001,7 +1001,7 @@ char IsCurrentAvailable(void) {
     bool must_be_single_phase_charging = (EnableC2 == ALWAYS_OFF || (Mode == MODE_SOLAR && EnableC2 == SOLAR_OFF) ||
             (Mode == MODE_SOLAR && EnableC2 == AUTO && Nr_Of_Phases_Charging == 1));
     int Phases = must_be_single_phase_charging ? 1 : 3;
-    if ((MaxSumMains && Phases * ActiveEVSE * (MinCurrent * 10) + Isum) > (MaxSumMains * 10)) {
+    if ((MaxSumMains != 0) && (((Phases * ActiveEVSE * MinCurrent * 10) + Isum) > (MaxSumMains * 10))) {
         _LOG_D("#%d MaxSumMains. ActiveEVSE=%i, MinCurrent=%iA, Isum=%.1fA, MaxSumMains=%iA.\n", __LINE__, ActiveEVSE, MinCurrent,  (float)Isum/10, MaxSumMains);
         return 0;                                                           // Not enough current available!, return with error
     }
@@ -1195,7 +1195,7 @@ void CalcBalancedCurrent(char mod) {
         else {
             Iheadroom = (MaxMains * 10) - MainsMeter.Imeasured;
         }
-        if (MaxSumMains && Iheadroom > ((MaxSumMains * 10) - Isum)/Temp_Phases) {
+        if ((MaxSumMains != 0) && (Iheadroom > (((MaxSumMains * 10) - Isum)/Temp_Phases))) {
             Iheadroom = ((MaxSumMains * 10) - Isum)/Temp_Phases;
             LimitedByMaxSumMains = true;
             _LOG_V("Current is limited by MaxSumMains: MaxSumMains=%iA, Isum=%.1fA, Temp_Phases=%i.\n", MaxSumMains, (float)Isum/10, Temp_Phases);
