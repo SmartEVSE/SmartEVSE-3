cb94dc6b37036d98cef30fc68372e554afee9cd3
dingo35 <spaarder@hotmail.nl>
2024-12-26 15:56:06 +0100

    common.h, common.cpp, main.cpp: move Timer10ms to common.cpp

--- SmartEVSE-3/include/common.h
+++ SmartEVSE-3/include/common.h
@@ -3,12 +3,18 @@
 
 #ifdef SMARTEVSE_VERSION //v3 and v4
 #include <Arduino.h>
+#include "glcd.h"
 #endif
 
 #include "debug.h"
 #include "stdint.h"
 #include "common_c.h"
 
+#if !defined(SMARTEVSE_VERSION) || SMARTEVSE_VERSION == 3 //CH32 and v3
+//#include "meter.h"
+#endif
+
+//here should only be declarations for code that will not run on the CH32
 class Button {
   public:
     void CheckSwitch(bool force = false);
@@ -22,6 +28,10 @@ class Button {
 
 extern Button ExtSwitch;
 
+extern void getButtonState();
+extern void PowerPanicESP();
+
+extern uint8_t LCDlock, MainVersion;
 enum Single_Phase_t { FALSE, GOING_TO_SWITCH, AFTER_SWITCH };
 
 #endif
