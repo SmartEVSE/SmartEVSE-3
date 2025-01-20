c301cda93ceceeb787eeca77c262b1c79e82a0a9
dingo35 <spaarder@hotmail.nl>
2025-01-20 19:43:00 +0100

    modbus.h: integrate routines

--- SmartEVSE-3/src/modbus.h
+++ SmartEVSE-3/src/modbus.h
@@ -30,6 +30,7 @@
 #include "meter.h"
 #include "ModbusServerRTU.h"
 #include "ModbusClientRTU.h"
+#endif
 
 struct ModBus {
     uint8_t Address;
@@ -46,6 +47,7 @@ struct ModBus {
     uint8_t Exception;
 };
 
+#ifdef SMARTEVSE_VERSION //ESP32
 extern struct ModBus MB;
 
 // definition of MBserver / MBclient class is done in evse.cpp
@@ -53,87 +55,16 @@ extern ModbusServerRTU MBserver;
 extern ModbusClientRTU MBclient; 
 
 void RS485SendBuf(uint8_t *buffer, uint8_t len);
-uint8_t mapModbusRegister2ItemID();
 
-// ########################### Modbus main functions ###########################
-
-void ModbusReadInputRequest(uint8_t address, uint8_t function, uint16_t reg, uint16_t quantity);
-void ModbusReadInputResponse(uint8_t address, uint8_t function, uint16_t *values, uint8_t count);
-void ModbusWriteSingleRequest(uint8_t address, uint16_t reg, uint16_t value);
-void ModbusWriteMultipleRequest(uint8_t address, uint16_t reg, uint16_t *values, uint8_t count);
-void ModbusException(uint8_t address, uint8_t function, uint8_t exception);
-void ModbusDecode(uint8_t *buf, uint8_t len);
-
-// ########################### EVSE modbus functions ###########################
-
-void requestMeasurement(uint8_t Meter, uint8_t Address, uint16_t Register, uint8_t Count);
-void requestCurrentMeasurement(uint8_t Meter, uint8_t Address);
-
-//void ReadItemValueResponse(void);
-//void WriteItemValueResponse(void);
-//void WriteMultipleItemValueResponse(void);
 #else //SMARTEVSE_VERSION
-/*
-;	 Project:       Smart EVSE
-;
-;
-;
-; Permission is hereby granted, free of charge, to any person obtaining a copy
-; of this software and associated documentation files (the "Software"), to deal
-; in the Software without restriction, including without limitation the rights
-; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-; copies of the Software, and to permit persons to whom the Software is
-; furnished to do so, subject to the following conditions:
-;
-; The above copyright notice and this permission notice shall be included in
-; all copies or substantial portions of the Software.
-;
-; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-; THE SOFTWARE.
-*/
 
-struct ModBus {
-    uint8_t Address;
-    uint8_t Function;
-    uint16_t Register;
-    uint16_t RegisterCount;
-    uint16_t Value;
-    uint8_t *Data;
-    uint8_t DataLength;
-    uint8_t Type;
-    uint8_t RequestAddress;
-    uint8_t RequestFunction;
-    uint16_t RequestRegister;
-    uint8_t Exception;
-};
-
-
-uint8_t mapModbusRegister2ItemID();
-
-// ########################### Modbus main functions ###########################
-
-void ModbusReadInputRequest(uint8_t address, uint8_t function, uint16_t reg, uint16_t quantity);
-void ModbusReadInputResponse(uint8_t address, uint8_t function, uint16_t *values, uint8_t count);
-void ModbusWriteSingleRequest(uint8_t address, uint16_t reg, uint16_t value);
 void ModbusWriteSingleResponse(uint8_t address, uint16_t reg, uint16_t value);
-void ModbusWriteMultipleRequest(uint8_t address, uint16_t reg, uint16_t *values, uint8_t count);
 void ModbusWriteMultipleResponse(uint8_t address, uint16_t reg, uint16_t count);
-void ModbusException(uint8_t address, uint8_t function, uint8_t exception);
-void ModbusDecode(uint8_t *buf, uint8_t len);
 
-// ########################### EVSE modbus functions ###########################
-
-void requestMeasurement(uint8_t Meter, uint8_t Address, uint16_t Register, uint8_t Count);
 signed int receiveMeasurement(uint8_t *buf, uint8_t pos, uint8_t Endianness, MBDataType dataType, signed char Divisor);
 signed int receiveEnergyMeasurement(uint8_t *buf, uint8_t Meter);
 void requestPowerMeasurement(uint8_t Meter, uint8_t Address);
 signed int receivePowerMeasurement(uint8_t *buf, uint8_t Meter);
-void requestCurrentMeasurement(uint8_t Meter, uint8_t Address);
 uint8_t receiveCurrentMeasurement(uint8_t *buf, uint8_t Meter, int32_t *var);
 
 void ReadItemValueResponse(void);
@@ -142,5 +73,16 @@ void WriteMultipleItemValueResponse(void);
 
 
 #endif
+uint8_t mapModbusRegister2ItemID();
+
+void ModbusReadInputRequest(uint8_t address, uint8_t function, uint16_t reg, uint16_t quantity);
+void ModbusReadInputResponse(uint8_t address, uint8_t function, uint16_t *values, uint8_t count);
+void ModbusWriteSingleRequest(uint8_t address, uint16_t reg, uint16_t value);
+void ModbusWriteMultipleRequest(uint8_t address, uint16_t reg, uint16_t *values, uint8_t count);
+void ModbusException(uint8_t address, uint8_t function, uint8_t exception);
+void ModbusDecode(uint8_t *buf, uint8_t len);
+
+void requestMeasurement(uint8_t Meter, uint8_t Address, uint16_t Register, uint8_t Count);
+void requestCurrentMeasurement(uint8_t Meter, uint8_t Address);
 
 #endif
