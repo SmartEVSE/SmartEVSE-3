0d7ea88edcdbdf3e09bacd5c82ee69e332d0bfa1
dingo35 <spaarder@hotmail.nl>
2025-01-20 17:19:10 +0100

    modbus.cpp: integrate routines

--- SmartEVSE-3/src/modbus.cpp
+++ SmartEVSE-3/src/modbus.cpp
@@ -21,9 +21,10 @@
 ; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ; THE SOFTWARE.
 */
-#ifdef SMARTEVSE_VERSION //ESP32
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef SMARTEVSE_VERSION //ESP32
 #include "driver/uart.h"
 
 #include "modbus.h"
@@ -485,33 +486,8 @@ void WriteMultipleItemValueResponse(void) {
 }
 */
 #else //SMARTEVSE_VERSION CH32
-/*
-;	 Project:       Smart EVSE
-;
-;
-;
-; Permission is hereby granted, free of charge, to any person obtaining a copy
-; of this software and associated documentation files (the "Software"), to deal
-; in the Software without restriction, including without limitation the rights
-; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-; copies of the Software, and to permit persons to whom the Software is
-; furnished to do so, subject to the following conditions:
-;
-; The above copyright notice and this permission notice shall be included in
-; all copies or substantial portions of the Software.
-;
-; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-; THE SOFTWARE.
-*/
 
 #include "ch32v003fun.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include "main_c.h"
 #include "ch32.h"
 #include "modbus.h"
