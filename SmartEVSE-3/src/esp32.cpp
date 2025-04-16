SmartEVSE/SmartEVSE-3

commit d0f1e3e953c199029b1c78acc429ca8304d239e7

introduce Pause AccessStatus

SmartEVSE-3/src/esp32.cpp
@@ -134,7 +134,6 @@ extern const char StrStateName[15][13];
 const char StrStateNameWeb[15][17] = {"Ready to Charge", "Connected to EV", "Charging", "D", "Request State B", "State B OK", "Request State C", "State C OK", "Activate", "Charging Stopped", "Stop Charging", "Modem Setup", "Modem Request", "Modem Done", "Modem Denied"};
 const char StrErrorNameWeb[9][20] = {"None", "No Power Available", "Communication Error", "Temperature High", "EV Meter Comm Error", "RCM Tripped", "Waiting for Solar", "Test IO", "Flash Error"};
 const char StrMode[3][8] = {"Normal", "Smart", "Solar"};
-const char StrAccessBit[2][6] = {"Deny", "Allow"};
 const char StrRFIDStatusWeb[8][20] = {"Ready to read card","Present", "Card Stored", "Card Deleted", "Card already stored", "Card not in storage", "Card Storage full", "Invalid" };
 
 // Global data
@@ -567,7 +566,7 @@ void mqtt_receive_callback(const String topic, const String payload) {
             ToModemWaitStateTimer = 0;
             ToModemDoneStateTimer = 0;
             LeaveModemDoneStateTimer = 0;
-            setAccess(0);
+            setAccess(OFF);
         } else if (payload == "Normal") {
             setMode(MODE_NORMAL);
         } else if (payload == "Solar") {
@@ -576,6 +575,8 @@ void mqtt_receive_callback(const String topic, const String payload) {
         } else if (payload == "Smart") {
             setOverrideCurrent(0);
             setMode(MODE_SMART);
+        } else if (payload == "Pause") {
+            setAccess(PAUSE);
         }
     } else if (topic == MQTTprefix + "/Set/CustomButton") {
         if (payload == "On") {
@@ -890,12 +891,12 @@ void mqttPublishData() {
             MQTTclient.publish(MQTTprefix + "/EVCurrentL3", EVMeter.Irms[2], false, 0);
         }
         MQTTclient.publish(MQTTprefix + "/ESPTemp", TempEVSE, false, 0);
-        MQTTclient.publish(MQTTprefix + "/Mode", Access_bit == 0 ? "Off" : Mode > 3 ? "N/A" : StrMode[Mode], true, 0);
+        MQTTclient.publish(MQTTprefix + "/Mode", AccessStatus == OFF ? "Off" : AccessStatus == PAUSE ? "Pause" : Mode > 3 ? "N/A" : StrMode[Mode], true, 0);
         MQTTclient.publish(MQTTprefix + "/MaxCurrent", MaxCurrent * 10, true, 0);
         MQTTclient.publish(MQTTprefix + "/CustomButton", CustomButton ? "On" : "Off", false, 0);
         MQTTclient.publish(MQTTprefix + "/ChargeCurrent", Balanced0, true, 0);
         MQTTclient.publish(MQTTprefix + "/ChargeCurrentOverride", OverrideCurrent, true, 0);
-        MQTTclient.publish(MQTTprefix + "/Access", StrAccessBit[Access_bit], true, 0);
+        MQTTclient.publish(MQTTprefix + "/Access", AccessStatus == OFF ? "Deny" : AccessStatus == ON ? "Allow" : AccessStatus == PAUSE ? "Pause" : "N/A", true, 0);
         MQTTclient.publish(MQTTprefix + "/RFID", !RFIDReader ? "Not Installed" : RFIDstatus >= 8 ? "NOSTATUS" : StrRFIDStatusWeb[RFIDstatus], true, 0);
         if (RFIDReader && RFIDReader != 6) { //RFIDLastRead not updated in Remote/OCPP mode
             char buf[13];
@@ -1007,7 +1008,7 @@ void read_settings() {
         Config = preferences.getUChar("Config", CONFIG); 
         Lock = preferences.getUChar("Lock", LOCK); 
         Mode = preferences.getUChar("Mode", MODE); 
-        Access_bit = preferences.getUChar("Access", ACCESS_BIT);
+        AccessStatus = (AccessStatus_t) preferences.getUChar("Access", ON);
         if (preferences.isKey("CardOffset")) {
             CardOffset = preferences.getUChar("CardOffset", CARD_OFFSET);
             //write the old 8 bits value to the new 16 bits value
@@ -1082,7 +1083,7 @@ void write_settings(void) {
     preferences.putUChar("Config", Config); 
     preferences.putUChar("Lock", Lock); 
     preferences.putUChar("Mode", Mode); 
-    preferences.putUChar("Access", Access_bit);
+    preferences.putUChar("Access", AccessStatus);
     preferences.putUShort("CardOffs16", CardOffset);
     preferences.putUChar("LoadBl", LoadBl); 
     preferences.putUShort("MaxMains", MaxMains); 
@@ -1180,9 +1181,12 @@ bool handle_URI(struct mg_connection *c, struct mg_http_message *hm,  webServerR
       if (!memcmp("GET", hm->method.buf, hm->method.len)) {                     // if GET
         String mode = "N/A";
         int modeId = -1;
-        if(Access_bit == 0)  {
+        if(AccessStatus == OFF)  {
             mode = "OFF";
             modeId=0;
+        } else if(AccessStatus == PAUSE)  {
+            mode = "PAUSE";
+            modeId=4;
         } else {
             switch(Mode) {
                 case MODE_NORMAL: mode = "NORMAL"; modeId=1; break;
@@ -1236,7 +1240,7 @@ bool handle_URI(struct mg_connection *c, struct mg_http_message *hm,  webServerR
         doc["evse"]["temp"] = TempEVSE;
         doc["evse"]["temp_max"] = maxTemp;
         doc["evse"]["connected"] = evConnected;
-        doc["evse"]["access"] = Access_bit == 1;
+        doc["evse"]["access"] = AccessStatus;
         doc["evse"]["mode"] = Mode;
         doc["evse"]["loadbl"] = LoadBl;
         doc["evse"]["pwm"] = CurrentPWM;
@@ -1441,7 +1445,7 @@ bool handle_URI(struct mg_connection *c, struct mg_http_message *hm,  webServerR
                 if (!StoreTimeString(DelayedStartTimeStr, &DelayedStartTime)) {
                     //parse OK
                     if (DelayedStartTime.diff > 0)
-                        setAccess(0);                         //switch to OFF, we are Delayed Charging
+                        setAccess(OFF);                         //switch to OFF, we are Delayed Charging
                     else {//we are in the past so no delayed charging
                         DelayedStartTime.epoch2 = DELAYEDSTARTTIME;
                         DelayedStopTime.epoch2 = DELAYEDSTOPTIME;
@@ -1492,7 +1496,7 @@ bool handle_URI(struct mg_connection *c, struct mg_http_message *hm,  webServerR
                     ToModemDoneStateTimer = 0;
                     LeaveModemDoneStateTimer = 0;
                     LeaveModemDeniedStateTimer = 0;
-                    setAccess(0);
+                    setAccess(OFF);
                     break;
                 case 1:
                     setMode(MODE_NORMAL);
@@ -1502,6 +1506,9 @@ bool handle_URI(struct mg_connection *c, struct mg_http_message *hm,  webServerR
                     break;
                 case 3:
                     setMode(MODE_SMART);
+                    break;
+                case 4: // PAUSE
+                    setAccess(PAUSE);
                     break;
                 default:
                     mode = "Value not allowed!";
@@ -2268,7 +2275,7 @@ void ocppDeinit() {
 
     if (OcppTrackPermitsCharge) {
         _LOG_A("OCPP unset Access_bit\n");
-        setAccess(false);
+        setAccess(OFF);
     }
 
     OcppTrackPermitsCharge = false;
@@ -2326,21 +2333,21 @@ void ocppLoop() {
         // RFID reader in OCPP mode or RFID fully disabled - OCPP controls Access_bit
         if (!OcppTrackPermitsCharge && ocppPermitsCharge()) {
             _LOG_A("OCPP set Access_bit\n");
-            setAccess(true);
-        } else if (Access_bit && !ocppPermitsCharge()) {
+            setAccess(ON);
+        } else if (AccessStatus == OFF && !ocppPermitsCharge()) {
             _LOG_A("OCPP unset Access_bit\n");
-            setAccess(false);
+            setAccess(OFF);
         }
         OcppTrackPermitsCharge = ocppPermitsCharge();
 
         // Check if OCPP charge permission has been revoked by other module
         if (OcppTrackPermitsCharge && // OCPP has set Acess_bit and still allows charge
-                !Access_bit) { // Access_bit is not active anymore
+                AccessStatus == OFF) { // Access_bit is not active anymore
             endTransaction(nullptr, "Other");
         }
     } else {
         // Built-in RFID store enabled - OCPP does not control Access_bit, but starts transactions when Access_bit is set
-        if (Access_bit && !OcppTrackAccessBit && !getTransaction() && isOperative()) {
+        if ((AccessStatus == ON || AccessStatus == PAUSE ) && !OcppTrackAccessBit && !getTransaction() && isOperative()) {
             // Access_bit has been set
             OcppTrackAccessBit = true;
             _LOG_A("OCPP detected Access_bit set\n");
@@ -2351,7 +2358,7 @@ void ocppLoop() {
                 sprintf(buf, "%02X%02X%02X%02X%02X%02X%02X", RFID[0], RFID[1], RFID[2], RFID[3], RFID[4], RFID[5], RFID[6]);
             }
             beginTransaction_authorized(buf);
-        } else if (!Access_bit && (OcppTrackAccessBit || (getTransaction() && getTransaction()->isActive()))) {
+        } else if (AccessStatus == OFF && (OcppTrackAccessBit || (getTransaction() && getTransaction()->isActive()))) {
             OcppTrackAccessBit = false;
             _LOG_A("OCPP detected Access_bit unset\n");
             char buf[15];
@@ -2872,8 +2879,8 @@ void loop() {
             time_t now = time(nullptr);             //get current local time
             DelayedStartTime.diff = DelayedStartTime.epoch2 - (mktime(localtime(&now)) - EPOCH2_OFFSET);
             if (DelayedStartTime.diff > 0) {
-                if (Access_bit != 0 && (DelayedStopTime.epoch2 == 0 || DelayedStopTime.epoch2 > DelayedStartTime.epoch2))
-                    setAccess(0);                         //switch to OFF, we are Delayed Charging
+                if (AccessStatus != OFF && (DelayedStopTime.epoch2 == 0 || DelayedStopTime.epoch2 > DelayedStartTime.epoch2))
+                    setAccess(OFF);                         //switch to OFF, we are Delayed Charging
             }
             else {
                 //starttime is in the past so we are NOT Delayed Charging, or we are Delayed Charging but the starttime has passed!
@@ -2881,7 +2888,7 @@ void loop() {
                     DelayedStartTime.epoch2 += 24 * 3600;                           //add 24 hours so we now have a new starttime
                 else
                     DelayedStartTime.epoch2 = DELAYEDSTARTTIME;
-                setAccess(1);
+                setAccess(ON);
             }
         }
         //only update StopTime.diff if starttime has already passed
@@ -2895,7 +2902,7 @@ void loop() {
                     DelayedStopTime.epoch2 += 24 * 3600;                        //add 24 hours so we now have a new starttime
                 else
                     DelayedStopTime.epoch2 = DELAYEDSTOPTIME;
-                setAccess(0);                         //switch to OFF
+                setAccess(OFF);                         //switch to OFF
             }
         }
         //_LOG_A("DINGO: firmwareUpdateTimer just before decrement=%i.\n", firmwareUpdateTimer);

SmartEVSE-3/data/index.html
@@ -59,7 +59,7 @@
               }
               $('#required_evccid').val(data.settings.required_evccid || "");
             }
-            for(let x=0; x<=3; x++) {
+            for(let x=0; x<=4; x++) {
               if(x == data.mode_id) {
                 $('#mode_' + x).css("background-color", "#1CC88A");
               } else {
@@ -1129,6 +1129,7 @@
                                         <div class="col">
                                           <form class="form-inline" id="form_mode">
                                             <button id="mode_0" onclick="activate('0')" style="width: 100px; display:inline-block;">OFF</button>
+                                            <button id="mode_4" onclick="activate('4')" style="width: 100px; display:inline-block;">PAUSE</button>
                                             <button id="mode_1" onclick="activate('1')" style="width: 120px; display:inline-block;">NORMAL</button>
                                             <div id="with_mainsmeter" style="display: inline">
                                               <button id="mode_2" onclick="activate('2')" style="width: 100px; display:inline-block;">SOLAR</button>

SmartEVSE-3/src/OneWire.cpp
@@ -284,9 +284,9 @@ void CheckRFID(void) {
                         x = MatchRFID();
                         if (x && !RFIDstatus) {
                             _LOG_A("RFID card found!\n");
-                            if (Access_bit) {
-                                setAccess(false);                                   // Access Off, Switch back to state B1/C1
-                            } else setAccess(true);
+                            if (AccessStatus == ON) {
+                                setAccess(OFF);                                     // Access Off, Switch back to state B1/C1
+                            } else setAccess(ON);
 
                             RFIDstatus = 1;
                         }  else if (!x) RFIDstatus = 7;                             // invalid card
@@ -296,11 +296,11 @@ void CheckRFID(void) {
                         x = MatchRFID();
                         if (x && !RFIDstatus) {
                             _LOG_A("RFID card found!\n");
-                            if (!Access_bit) {
+                            if (AccessStatus == OFF) {
                                 CardOffset = x;                                     // store cardoffset from current card
-                                setAccess(true);                                    // Access On
+                                setAccess(ON);                                      // Access On
                             } else if (CardOffset == x) {
-                                setAccess(false);                                   // Access Off, Switch back to state B1/C1
+                                setAccess(OFF);                                     // Access Off, Switch back to state B1/C1
                             }
                             RFIDstatus = 1;
                         }  else if (!x) RFIDstatus = 7;                             // invalid card

SmartEVSE-3/src/esp32.h
@@ -135,7 +135,7 @@ extern uint8_t ButtonState;                                                     
 extern uint8_t OldButtonState;                                                  // Holds previous push Buttons state (LSB 2:0)
 extern uint8_t ChargeDelay;                                                     // Delays charging in seconds.
 extern uint8_t TestState;
-extern uint8_t Access_bit;
+extern AccessStatus_t AccessStatus;
 extern uint16_t CardOffset;
 
 extern uint16_t SolarStopTimer;
@@ -225,7 +225,7 @@ void read_settings();
 void write_settings(void);
 void setSolarStopTimer(uint16_t Timer);
 void setState(uint8_t NewState);
-void setAccess(bool Access);
+void setAccess(AccessStatus_t Access);
 void setOverrideCurrent(uint16_t Current);
 void SetCPDuty(uint32_t DutyCycle);
 uint8_t setItemValue(uint8_t nav, uint16_t val);

SmartEVSE-3/src/glcd.cpp
@@ -586,7 +586,7 @@ extern uint16_t Balanced0;
     }
 
                                                                                 // MODE NORMAL
-    if (Mode == MODE_NORMAL || !Access_bit) {
+    if (Mode == MODE_NORMAL || AccessStatus == OFF) {
 
         glcd_clrln(0, 0x00);
         glcd_clrln(1, 0x04);                                                    // horizontal line
@@ -667,13 +667,15 @@ extern uint16_t Balanced0;
             sprintf(Str, "%u.%uA",Balanced0 / 10, Balanced0 % 10);
             GLCD_print_buf2(4, Str);
         } else {                                                                // STATE A and STATE B
-            if (Access_bit) {
+            if (AccessStatus == ON) {
                 GLCD_print_buf2(2, (const char *) "READY TO");
                 sprintf(Str, "CHARGE %u", ChargeDelay);
                 if (ChargeDelay) {
                     // BacklightTimer = BACKLIGHT;
                 } else Str[6] = '\0';
                 GLCD_print_buf2(4, Str);
+            } else if (AccessStatus == PAUSE) {
+                GLCD_print_buf2(2, (const char *) "PAUSE");
             } else {
 #if ENABLE_OCPP && defined(SMARTEVSE_VERSION) //run OCPP only on ESP32
                 if (OcppMode &&                                  // OCPP enabled
@@ -873,6 +875,8 @@ extern uint16_t Balanced0;
         } else if (State == STATE_MODEM_REQUEST || State == STATE_MODEM_WAIT || State == STATE_MODEM_DONE) {                                          // Modem states
             GLCD_print_buf2(5, (const char *) "MODEM");
 #endif
+        } else if (AccessStatus == PAUSE) {
+                    GLCD_print_buf2(5, "PAUSE");
         } else if (State != STATE_C) {
                 switch (Switching_To_Single_Phase) {
                     case FALSE:
@@ -1181,7 +1185,7 @@ void GLCDMenu(uint8_t Buttons) {
         ButtonTimer = millis();
     } else if (LCDNav == MENU_OFF && ((ButtonTimer + 2000) < millis() )) {
         LCDNav = 0;                                                             // Charging canceled
-        setAccess(false);
+        setAccess(OFF);
         ButtonRelease = 1;
     } else if ((LCDNav == MENU_OFF) && (Buttons == 0x7)) {                      // Button 1 released before entering menu?
         //if < button is pressed shorter then 2 seconds we are switching from Smart mode to Solar mode and vice versa
@@ -1191,12 +1195,12 @@ void GLCDMenu(uint8_t Buttons) {
         ButtonRelease = 0;
         GLCD();
     // start charging if > button is pressed longer then 2 seconds
-    } else if ((Access_bit == 0) && (LCDNav == 0) && (Buttons == 0x3) && (ButtonRelease == 0)) {     // Button 3 pressed ?
+    } else if ((AccessStatus == OFF) && (LCDNav == 0) && (Buttons == 0x3) && (ButtonRelease == 0)) {     // Button 3 pressed ?
         LCDNav = MENU_ON;                                                       // about to start charging
         ButtonTimer = millis();
     } else if (LCDNav == MENU_ON && ((ButtonTimer + 2000) < millis() )) {
         LCDNav = 0;                                                             // Charging canceled
-        setAccess(true);
+        setAccess(ON);
         ButtonRelease = 1;
     } else if ((LCDNav == MENU_ON) && (Buttons == 0x7)) {                      // Button 1 released before entering menu?
         LCDNav = 0;

SmartEVSE-3/src/main.cpp
@@ -196,8 +196,7 @@ bool PilotDisconnected = false;
 uint8_t PilotDisconnectTime = 0;                                            // Time the Control Pilot line should be disconnected (Sec)
 #endif
 uint8_t NodeNewMode = 0;
-uint8_t Access_bit = 0;                                                     // 0:No Access 1:Access to SmartEVSE
-
+AccessStatus_t AccessStatus = OFF;                                          // 0: OFF, 1: ON, 2: PAUSE
 uint8_t ConfigChanged = 0;
 
 uint16_t SolarStopTimer = 0;
@@ -342,11 +341,11 @@ void Button::HandleSwitch(void) {
         // Switch input pulled low
         switch (Switch) {
             case 1: // Access Button
-                setAccess(!Access_bit);                             // Toggle Access bit on/off
-                _LOG_I("Access: %d\n", Access_bit);
+                setAccess(AccessStatus == ON ? OFF : ON);           // Toggle AccessStatus OFF->ON->OFF (old behaviour) or PAUSE->ON
+                _LOG_I("Access: %d\n", AccessStatus);
                 break;
             case 2: // Access Switch
-                setAccess(true);
+                setAccess(ON);
                 break;
             case 3: // Smart-Solar Button
                 break;
@@ -385,7 +384,7 @@ void Button::HandleSwitch(void) {
         // Switch input released
         switch (Switch) {
             case 2: // Access Switch
-                setAccess(false);
+                setAccess(OFF);
                 break;
             case 3: // Smart-Solar Button
                 if (millis() < TimeOfPress + 1500) {                            // short press
@@ -488,7 +487,7 @@ void setMode(uint8_t NewMode) {
         return;
 
     // Take care of extra conditionals/checks for custom features
-    setAccess(!DelayedStartTime.epoch2); //if DelayedStartTime not zero then we are Delayed Charging
+    setAccess(DelayedStartTime.epoch2 ? OFF : ON); //if DelayedStartTime not zero then we are Delayed Charging
     if (NewMode == MODE_SOLAR) {
         // Reset OverrideCurrent if mode is SOLAR
         setOverrideCurrent(0);
@@ -503,7 +502,7 @@ void setMode(uint8_t NewMode) {
         if ((Mode != MODE_SOLAR && NewMode == MODE_SOLAR) || (Mode == MODE_SOLAR && NewMode != MODE_SOLAR)) {
             //we are switching from non-solar to solar
             //since we EnableC2 == SOLAR_OFF C2 is turned On now, and should be turned off
-            setAccess(0);                                                       //switch to OFF
+            setAccess(OFF);                                                     //switch to OFF
             switchOnLater = true;
         }
     }
@@ -525,7 +524,7 @@ void setMode(uint8_t NewMode) {
     SEND_TO_CH32(Mode); //d
 
     if (switchOnLater)
-        setAccess(1);
+        setAccess(ON);
 
     //make mode and start/stoptimes persistent on reboot
     if (preferences.begin("settings", false) ) {                        //false = write mode
@@ -811,13 +810,13 @@ void setState(uint8_t NewState) { //c
 
 // same for Mode/setMode
 
-void setAccess(bool Access) { //c
+void setAccess(AccessStatus_t Access) { //c
 #ifdef SMARTEVSE_VERSION //v3 and v4
-    Access_bit = Access;
+    AccessStatus = Access;
 #if SMARTEVSE_VERSION >= 40
-    Serial1.printf("Access@%u\n", Access_bit); //d
+    Serial1.printf("Access@%u\n", AccessStatus); //d
 #endif
-    if (Access == 0) {
+    if (Access == OFF || Access == PAUSE) {
         //TODO:setStatePowerUnavailable() ?
         if (State == STATE_C) setState(STATE_C1);                               // Determine where to switch to.
         else if (State != STATE_C1 && (State == STATE_B || State == STATE_MODEM_REQUEST || State == STATE_MODEM_WAIT || State == STATE_MODEM_DONE || State == STATE_MODEM_DENIED)) setState(STATE_B1);
@@ -825,7 +824,7 @@ void setAccess(bool Access) { //c
 
     //make mode and start/stoptimes persistent on reboot
     if (preferences.begin("settings", false) ) {                        //false = write mode
-        preferences.putUChar("Access", Access_bit);
+        preferences.putUChar("Access", AccessStatus);
         preferences.putUShort("CardOffs16", CardOffset);
         preferences.end();
     }
@@ -1525,7 +1524,7 @@ void Timer1S_singlerun(void) {
 
     if (AccessTimer && State == STATE_A) {
         if (--AccessTimer == 0) {
-            setAccess(false);                                           // re-lock EVSE
+            setAccess(OFF);                                             // re-lock EVSE
         }
     } else AccessTimer = 0;                                             // Not in state A, then disable timer
 
@@ -2068,7 +2067,7 @@ void CheckSerialComm(void) {
     SET_ON_RECEIVE(Config@, Config)
     SET_ON_RECEIVE(Lock@, Lock)
     SET_ON_RECEIVE(Mode@, Mode)
-    SET_ON_RECEIVE(Access@, Access_bit)
+    SET_ON_RECEIVE(Access@, AccessStatus)
     SET_ON_RECEIVE(OverrideCurrent@, OverrideCurrent)
     SET_ON_RECEIVE(LoadBl@, LoadBl)
     SET_ON_RECEIVE(MaxMains@, MaxMains)
@@ -2161,7 +2160,7 @@ static unsigned int locktimer = 0, unlocktimer = 0;
     // Check if the cable lock is used
     if (!Config && Lock) {                                      // Socket used and Cable lock enabled?
         // UnlockCable takes precedence over LockCable
-        if ((RFIDReader == 2 && Access_bit == 0) ||             // One RFID card can Lock/Unlock the charging socket (like a public charging station)
+        if ((RFIDReader == 2 && AccessStatus == OFF) ||        // One RFID card can Lock/Unlock the charging socket (like a public charging station)
 #if ENABLE_OCPP && defined(SMARTEVSE_VERSION) //run OCPP only on ESP32
         (OcppMode &&!OcppForcesLock) ||
 #endif
@@ -2448,11 +2447,11 @@ static unsigned int LedPwm = 0;                                                /
         GreenPwm = 0;
         BluePwm = 0;
 #endif //ENABLE_OCPP
-    } else if (Access_bit == 0 && CustomButton) {
+    } else if (AccessStatus == OFF && CustomButton) {
         RedPwm = ColorCustom[0];
         GreenPwm = ColorCustom[1];
         BluePwm = ColorCustom[2];
-    } else if (Access_bit == 0 || State == STATE_MODEM_DENIED) {
+    } else if (AccessStatus == OFF || State == STATE_MODEM_DENIED) {
         RedPwm = ColorOff[0];
         GreenPwm = ColorOff[1];
         BluePwm = ColorOff[2];
@@ -2511,7 +2510,7 @@ static unsigned int LedPwm = 0;                                                /
 #if SMARTEVSE_VERSION >=40
 void SendConfigToCH32() {
     // send configuration to WCH IC
-    Serial1.printf("Access@%u\n", Access_bit);
+    Serial1.printf("Access@%u\n", AccessStatus);
     Serial1.printf("MainsMeterType@%u\n", MainsMeter.Type);
     Serial1.printf("MainsMAddress@%u\n", MainsMeter.Address);
     Serial1.printf("EVMeterType@%u\n", EVMeter.Type);
@@ -2634,13 +2633,13 @@ void Timer10ms_singlerun(void) {
         } else if (pilot == PILOT_12V) {                                    // Check if we are disconnected, or forced to State A, but still connected to the EV
             // If the RFID reader is set to EnableOne or EnableAll mode, and the Charging cable is disconnected
             // We start a timer to re-lock the EVSE (and unlock the cable) after 60 seconds.
-            if ((RFIDReader == 2 || RFIDReader == 1) && AccessTimer == 0 && Access_bit == 1) AccessTimer = RFIDLOCKTIME;
+            if ((RFIDReader == 2 || RFIDReader == 1) && AccessTimer == 0 && AccessStatus == ON) AccessTimer = RFIDLOCKTIME;
 
             if (State != STATE_A) setState(STATE_A);                        // reset state, incase we were stuck in STATE_COMM_B
             ChargeDelay = 0;                                                // Clear ChargeDelay when disconnected.
             if (!EVMeter.ResetKwh) EVMeter.ResetKwh = 1;                    // when set, reset EV kWh meter on state B->C change.
         } else if ( pilot == PILOT_9V && ErrorFlags == NO_ERROR
-            && ChargeDelay == 0 && Access_bit && State != STATE_COMM_B
+            && ChargeDelay == 0 && AccessStatus == ON && State != STATE_COMM_B
 #if MODEM
             && State != STATE_MODEM_REQUEST && State != STATE_MODEM_WAIT && State != STATE_MODEM_DONE   // switch to State B ?
 #endif
@@ -2674,7 +2673,7 @@ void Timer10ms_singlerun(void) {
             } else if (Mode == MODE_SOLAR) {                                // Not enough power:
                 ErrorFlags |= NO_SUN;                                       // Not enough solar power
             } else ErrorFlags |= LESS_6A;                                   // Not enough power available
-        } else if (pilot == PILOT_9V && State != STATE_B1 && State != STATE_COMM_B && Access_bit) {
+        } else if (pilot == PILOT_9V && State != STATE_B1 && State != STATE_COMM_B && AccessStatus == ON) {
             setState(STATE_B1);
         }
     } // State == STATE_A || State == STATE_COMM_B || State == STATE_B1
@@ -3134,9 +3133,7 @@ uint8_t setItemValue(uint8_t nav, uint16_t val) {
             if (LoadBl < 2) MainsMeter.setTimeout(COMM_TIMEOUT);                // reset timeout when register is written
             break;
         case STATUS_ACCESS:
-            if (val == 0 || val == 1) {
-                setAccess(val);
-            }
+            setAccess((AccessStatus_t) val);
             break;
 
         default:
@@ -3243,7 +3240,7 @@ uint16_t getItemValue(uint8_t nav) {
         case STATUS_SOLAR_TIMER:
             return SolarStopTimer;
         case STATUS_ACCESS:
-            return Access_bit;
+            return AccessStatus;
         case STATUS_CONFIG_CHANGED:
             return ConfigChanged;
 

SmartEVSE-3/src/main.h
@@ -115,7 +115,6 @@
 #define EMCUSTOM_EREGISTER 0
 #define EMCUSTOM_EDIVISOR 8
 #define RFID_READER 0
-#define ACCESS_BIT 1
 #define WIFI_MODE 0
 #define CARD_OFFSET 0
 #define ENABLE_C2 ALWAYS_ON
@@ -325,6 +324,7 @@ extern void PowerPanicESP();
 
 extern uint8_t LCDlock;
 enum Single_Phase_t { FALSE, GOING_TO_SWITCH, AFTER_SWITCH };
+enum AccessStatus_t { OFF, ON, PAUSE };
 extern void CalcBalancedCurrent(char mod);
 extern void write_settings(void);
 extern void CalcIsum(void);

commit 9662d0a8a743ed6612d6f77f9ed4bbf114d813a8

esp32.cpp: fix OCPP bug introduced in d0f1e3e953

SmartEVSE-3/src/esp32.cpp
@@ -2353,7 +2353,7 @@ void ocppLoop() {
         if (!OcppTrackPermitsCharge && ocppPermitsCharge()) {
             _LOG_A("OCPP set Access_bit\n");
             setAccess(ON);
-        } else if (AccessStatus == OFF && !ocppPermitsCharge()) {
+        } else if (AccessStatus == ON && !ocppPermitsCharge()) {
             _LOG_A("OCPP unset Access_bit\n");
             setAccess(OFF);
         }