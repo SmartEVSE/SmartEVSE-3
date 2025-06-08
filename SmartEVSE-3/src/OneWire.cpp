e35fc96ab6bdae0283a14b74f84835dd76d0de89
rob040 <72281835+rob040@users.noreply.github.com>
2025-06-08 19:25:20 +0200

    OneWire.cpp: expand RFIDid from 6 to 7 bytes, with bugfixes

--- SmartEVSE-3/src/OneWire.cpp
+++ SmartEVSE-3/src/OneWire.cpp
@@ -33,8 +33,10 @@
 #include "OneWire.h"
 #include "OneWireESP32.h"
 
+#define RFIDSIZE 700
+
 unsigned char RFID[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-unsigned char RFIDlist[600];                                                    // holds up to 100 RFIDs
+unsigned char RFIDlist[RFIDSIZE];                                               // holds up to 100 RFIDs of 7 bytes each
 
 OneWire32 ds(PIN_SW_IN, 0, 1, 0);                                               //gpio pin, tx, rx, parasite power
 
@@ -68,7 +70,7 @@ unsigned char OneWireReadCardId(void) {
             RFID[0] = 0;                                                        // CRC incorrect, clear first byte of RFID buffer
             return 0;
         } else {
-            for (x=1 ; x<7 ; x++) _LOG_A_NO_FUNC("%02x",RFID[x]);
+            for (x=0 ; x<7 ; x++) _LOG_A_NO_FUNC("%02x",RFID[x]);
             _LOG_A_NO_FUNC("\r\n");
             return 1;
         }
@@ -87,8 +89,8 @@ unsigned char OneWireReadCardId(void) {
 void WriteRFIDlist(void) {
 
     if (preferences.begin("RFIDlist", false) ) {                                // read/write
-        preferences.putBytes("RFID", RFIDlist, 600);                            // write 600 bytes to storage
-        preferences.putUChar("RFIDinit", 2);                                    // data initialized to 600byte mode
+        preferences.putBytes("RFID", RFIDlist, RFIDSIZE);                       // write 700 bytes to storage
+        preferences.putUChar("RFIDinit", 3);                                    // data initialized to 700byte mode
         preferences.end();
     } else {
         _LOG_A("Error opening preferences!\n");
@@ -98,18 +100,43 @@ void WriteRFIDlist(void) {
     _LOG_I("\nRFID list saved\n");
 }
 
-// Read a list of 20 RFID's from preferences
+
+// Expand the old 6 byte UID array to a 7 byte UID array
+//
+void expandUIDArray(unsigned char* oldArray, unsigned char* newArray, uint16_t oldSize) {
+    int oldIdx = 0;
+    int newIdx = 0;
+
+    while (oldIdx <= oldSize - 6 && newIdx <= RFIDSIZE - 7) {
+        memcpy(&newArray[newIdx], &oldArray[oldIdx], 6);
+
+        newArray[newIdx + 6] = 0xff;
+
+        oldIdx += 6;
+        newIdx += 7;
+    }
+
+    // If there's any remaining space in the new array, fill it with 0xFF
+    while (newIdx < RFIDSIZE) {
+        newArray[newIdx] = 0xff;
+        newIdx++;
+    }
+}
+
+
+// Read a list of 100 RFID's from preferences
 //
 void ReadRFIDlist(void) {
     uint8_t initialized = 0;
+    unsigned char RFIDold[600];                                                 // old RFID buffer
 
     if (preferences.begin("RFIDlist", true) ) {                                 // read only
         initialized = preferences.getUChar("RFIDinit", 0);
         switch (initialized) {
             case 1:                                                             // we were initialized to old 120 bytes mode
-                preferences.getBytes("RFID", RFIDlist, 120);                    // read 120 bytes from storage
-                //we are now going to convert from RFIDlist 120bytes to RFIDlist 600bytes
-                for (int i = 120; i < 600; i++) RFIDlist[i] = 0xff;
+                preferences.getBytes("RFID", RFIDold, 120);                     // read 120 bytes from storage
+                //we are now going to convert from RFIDold 120bytes to RFIDlist 700bytes
+                expandUIDArray(RFIDold, RFIDlist, 120);
                 preferences.remove("RFID");
                 WriteRFIDlist();
                 break;
@@ -117,11 +144,19 @@ void ReadRFIDlist(void) {
                 DeleteAllRFID();                                                // when unitialized, delete all cardIDs
                 setItemValue(MENU_RFIDREADER, 0);                               // RFID Reader Disabled
                 break;
-            case 2:                                                             // extended RFIDlist with room for 100tags of 6 bytes = 600 bytes
-                preferences.getBytes("RFID", RFIDlist, 600);                    // read 600 bytes from storage
+            case 2:
+                preferences.getBytes("RFID", RFIDold, 600);                     // expand v2 RFIDlist to 100 card id's with 7 bytes
+                //we are now going to convert from RFIDold 600bytes to RFIDlist 700bytes
+                expandUIDArray(RFIDold, RFIDlist, 600);
+                preferences.remove("RFID");
+                preferences.end();
+                WriteRFIDlist();
+                break;
+            case 3:                                                             // extended RFIDlist with room for 100tags of 7 bytes = 700 bytes
+                preferences.getBytes("RFID", RFIDlist, RFIDSIZE);               // read 700 bytes from storage
+                preferences.end();
                 break;
         }
-        preferences.end();
 
     } else {
         _LOG_A("Error opening preferences!\n") ;
@@ -129,16 +164,20 @@ void ReadRFIDlist(void) {
 }
 
 // scan for matching RFID in RFIDlist
-// returns offset+6 when found, 0 when not found
+// returns offset+7 when found, 0 when not found
 uint16_t MatchRFID(void) {
     uint16_t offset = 0, r;
 
     do {
-        r = memcmp(RFID + 1, RFIDlist + offset, 6);                            // compare read RFID with list of stored RFID's
-        offset += 6;
-    } while (r !=0 && offset < 594);
+        if (RFID[0] == 0x01) {  // old reader 6 byte UID starts at RFID[1]
+            r = memcmp(RFID + 1, RFIDlist + offset, 6);                        // compare only 6 bytes
+        } else {                // new reader 7 byte UID starts ar RFID[0]
+            r = memcmp(RFID, RFIDlist + offset, 7);                            // compare 7 bytes
+        }
+        offset += 7;
+    } while (r !=0 && offset < RFIDSIZE-7);
 
-    if (r == 0) return offset;                                                  // return offset + 6 in RFIDlist
+    if (r == 0) return offset;                                                  // return offset + 7 in RFIDlist
     else return 0;
 }
 
@@ -149,22 +188,26 @@ uint16_t MatchRFID(void) {
 // returns 0 when all slots are full.
 unsigned char StoreRFID(void) {
     uint16_t offset = 0, r;
-    unsigned char empty[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
+    unsigned char empty[7] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff};
 
     // first check if the Card ID was already stored.
     if ( MatchRFID() ) return 2;                                                // already stored, that's ok.
 
     do {
-        r = memcmp(empty, RFIDlist + offset, 6);
-        offset += 6;
-    } while (r !=0 && offset < 600);
+        r = memcmp(empty, RFIDlist + offset, 7);
+        offset += 7;
+    } while (r !=0 && offset < RFIDSIZE);
     if (r != 0) return 0;                                                       // no more room to store RFID
-    offset -= 6;
+    offset -= 7;
     _LOG_A("offset %u ",offset);
-    memcpy(RFIDlist + offset, RFID+1, 6);
+    if (RFID[0] == 0x01) {
+        memcpy(RFIDlist + offset, RFID+1, 6);                                   // Store 6 byte UID
+    } else {
+        memcpy(RFIDlist + offset, RFID, 7);                                     // Store 7 byte UID
+    }
 
     _LOG_I("\nRFIDlist:");
-    for (r=0; r<600; r++) _LOG_I_NO_FUNC("%02x",RFIDlist[r]);
+    for (r=0; r<RFIDSIZE; r++) _LOG_I_NO_FUNC("%02x",RFIDlist[r]);
 
     WriteRFIDlist();
     return 1;
@@ -184,12 +227,12 @@ unsigned char DeleteRFID(void) {
 
     offset = MatchRFID();                                                       // find RFID in list
     if (offset) {
-        offset -= 6;
-        for (r = 0; r < 6; r++) RFIDlist[offset + r] = 0xff;
+        offset -= 7;
+        for (r = 0; r < 7; r++) RFIDlist[offset + r] = 0xff;
     } else return 0;
 
     _LOG_A("deleted %u ",offset);
-    for (r=0; r<600; r++) _LOG_A("%02x",RFIDlist[r]);
+    for (r=0; r<RFIDSIZE; r++) _LOG_A_NO_FUNC("%02x",RFIDlist[r]);
 
     WriteRFIDlist();
     return 1;
@@ -198,7 +241,7 @@ unsigned char DeleteRFID(void) {
 void DeleteAllRFID(void) {
     uint16_t i;
 
-    for (i = 0; i < 600; i++) RFIDlist[i] = 0xff;
+    for (i = 0; i < RFIDSIZE; i++) RFIDlist[i] = 0xff;
     WriteRFIDlist();
     _LOG_I("All RFID cards erased!\n");
 }
@@ -215,7 +258,12 @@ void CheckRFID(void) {
 
                 static unsigned long lastread;
                 if (OcppMode && millis() - lastread > 1500) {                       // Debounce 1500ms
-                    ocppUpdateRfidReading(RFID + 1, 6); // UUID starts at RFID+1; Pad / truncate UUID to 6-bytes for now
+                    if (RFID[0] == 0x01) {
+                        ocppUpdateRfidReading(RFID + 1, 6); // UUID starts at RFID+1; Pad / truncate UUID to 6-bytes for now
+                    } else {
+                        ocppUpdateRfidReading(RFID, 7); // UID starts at RFID; 7 byte UID
+                    }
+
                     lastread = millis();
                 }
                 RFIDstatus = 1;
