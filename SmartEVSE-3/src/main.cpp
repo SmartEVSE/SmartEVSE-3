16003d7fdc5d5014d8fc9a83b5f4098c5ad850d6
rob040 <72281835+rob040@users.noreply.github.com>
2025-06-10 23:05:16 +0200

    main.cpp: timeout on Node if no Mainsmeter is defined

--- SmartEVSE-3/src/main.cpp
+++ SmartEVSE-3/src/main.cpp
@@ -2667,7 +2667,7 @@ uint8_t PollEVNode = NR_EVSES, updated = 0;
     while(1)  // infinite loop
     {
         // Check if the cable lock is used
-        if (Lock) {                                                 // Cable lock enabled?
+        if (!Config && Lock) {                                      // Socket used and Cable lock enabled?
             // UnlockCable takes precedence over LockCable
             if ((RFIDReader == 2 && Access_bit == 0) ||             // One RFID card can Lock/Unlock the charging socket (like a public charging station)
 #if ENABLE_OCPP
@@ -3555,16 +3555,27 @@ void Timer1S(void * parameter) {
              } else Node[x].IntTimer = 0;                                    // Reset IntervalTime when not charging
         }
 
-        if (MainsMeter.Type) {
+        // Mainsmeter defined, and power sharing set to Disabled or Master
+        if (MainsMeter.Type && LoadBl < 2) {
             if ( MainsMeter.Timeout == 0 && !(ErrorFlags & CT_NOCOMM) && Mode != MODE_NORMAL) { // timeout if current measurement takes > 10 secs
-                // In Normal mode do not timeout; there might be MainsMeter/EVMeter configured that can be retrieved through the API,
-                // but in Normal mode we just want to charge ChargeCurrent, irrespective of communication problems.
+                // When power sharing is set to Disabled/Master, and in Normal mode, do not timeout;
+                // there might be MainsMeter/EVMeter configured that can be retrieved through the API.
                 ErrorFlags |= CT_NOCOMM;
                 setStatePowerUnavailable();
 #ifdef SENSORBOX_VERSION // SB2
                 SB2.SoftwareVer = 0;
 #endif
                 _LOG_W("Error, MainsMeter communication error!\n");
+            } else {
+                if (MainsMeter.Timeout) MainsMeter.Timeout--;
+            }
+        // We are a Node, we will timeout if there is no communication with the Master controller.
+        } else if (LoadBl > 1) {
+            if (MainsMeter.Timeout == 0 && !(ErrorFlags & CT_NOCOMM)) {
+                ErrorFlags |= CT_NOCOMM;
+                setStatePowerUnavailable();
+                SB2.SoftwareVer = 0;
+                _LOG_W("Error, Master communication error!\n");
             } else {
                 if (MainsMeter.Timeout) MainsMeter.Timeout--;
             }
